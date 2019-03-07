da41bda43bcff22d768f33b9f664678d91bd3249
sean miller <millertime@seans-MacBook-Pro.local>
2019-03-07 12:26:40 -0800

    Doubly and Singly linked list

---
+++ Doubly_Linked_List/doubly_linked_list.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+
+  }
+
+  
+}
+
+let list = new DoublyLinkedList();
+let node = new Node(78);
+list.push(node);
+console.log(list);
+list.push(11);
+console.log(list)
