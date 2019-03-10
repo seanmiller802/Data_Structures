cc7dee2a74a09548b80d974f286f66f63190d677
sean miller <millertime@seans-MacBook-Pro.local>
2019-03-09 21:34:09 -0800

    Breadth first search

--- BST/binary_search_tree.js
+++ BST/binary_search_tree.js
@@ -10,46 +10,47 @@ class BinarySearchTree {
     constructor(){
         this.root = null;
     }
+
     insert(value){
-        var newNode = new Node(value);
-        if(this.root === null){
-            this.root = newNode;
-            return this;
-        }
-        var current = this.root;
-        while(true){
-            if(value === current.value) return undefined;
-            if(value < current.value){
-                if(current.left === null){
-                    current.left = newNode;
-                    return this;
-                }
-                current = current.left;
-            } else {
-                if(current.right === null){
-                    current.right = newNode;
-                    return this;
-                } 
-                current = current.right;
-            }
-        }
+      var newNode = new Node(value);
+      if(this.root === null){
+          this.root = newNode;
+          return this;
+      }
+      var current = this.root;
+      while(true){
+          if(value === current.value) return undefined;
+          if(value < current.value){
+              if(current.left === null){
+                  current.left = newNode;
+                  return this;
+              }
+              current = current.left;
+          } else {
+              if(current.right === null){
+                  current.right = newNode;
+                  return this;
+              } 
+              current = current.right;
+          }
+      }
     }
-    
+
     find(value){
-        if(this.root === null) return false;
-        var current = this.root,
-            found = false;
-        while(current && !found){
-            if(value < current.value){
-                current = current.left;
-            } else if(value > current.value){
-                current = current.right;
-            } else {
-                found = true;
-            }
-        }
-        if(!found) return undefined;
-        return current;
+      if(this.root === null) return false;
+      var current = this.root,
+          found = false;
+      while(current && !found){
+          if(value < current.value){
+              current = current.left;
+          } else if(value > current.value){
+              current = current.right;
+          } else {
+              found = true;
+          }
+      }
+      if(!found) return undefined;
+      return current;
     }
 
     contains(value){
@@ -67,6 +68,30 @@ class BinarySearchTree {
         }
         return false;
     }
+
+    BFS() {
+      var queue = [],
+          visited = [],
+          node = this.root;
+
+      queue.push(node);
+      
+      while(queue.length) {
+        var node = queue.shift();
+        visited.push(node.value);
+        if(node.left) {
+          queue.push(node.left);
+        }
+        if(node.right) {
+          queue.push(node.right);
+        }
+      }
+
+      console.log("DATA IS:", visited);
+      return visited;
+    }
+
+
 }
 
 
@@ -76,14 +101,14 @@ class BinarySearchTree {
 
 var tree = new BinarySearchTree();
 tree.insert(10)
-tree.insert(5)
-tree.insert(13)
-tree.insert(11)
-tree.insert(2)
-tree.insert(16)
-tree.insert(7)
-var found = tree.find(13);
-console.log(found);
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20);
+
+tree.BFS();
+
 
 
 
