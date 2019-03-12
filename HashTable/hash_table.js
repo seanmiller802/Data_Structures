b3d4e7dbfb845a936a5056bf97d3bb3b6a85b256
sean miller <millertime@seans-MacBook-Pro.local>
2019-03-11 19:08:43 -0700

    HashTable get and set methods

--- HashTable/hash_table.js
+++ HashTable/hash_table.js
@@ -3,6 +3,26 @@ class HashTable {
     this.keyMap = new Array(size);
   }
 
+  set(key, value) {
+    let hash = this._hash(key);
+    if(!this.keyMap[hash]) {
+      this.keyMap[hash] = [];
+    }
+    this.keyMap[hash].push([key, value]); 
+  }
+
+  get(key) {
+    let hash = this._hash(key);
+    if(this.keyMap[hash]) {
+      for(let i=0; i < this.keyMap[hash].length; i++) {
+        if(this.keyMap[hash][i][0] === key) {
+          return this.keyMap[hash][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
   _hash(key) {
     let total = 0;
     let WEIRD_PRIME = 31;
@@ -14,3 +34,13 @@ class HashTable {
      return total;
   }
 }
+
+let HT = new HashTable();
+HT.set("maroon", "#800000");
+HT.set("yellow", "#FFFF00");
+HT.set("olive", "#808000");
+HT.set("salmon", "#FA8072");
+HT.set("lightcoral", "#F08080");
+HT.set("mediumvioletred", "#C71585");
+HT.set("plum", "#DDA0DD");
+console.log(HT.get("maroon"));
