de04a4b8bf9d049870a845b3e900881b8b7c11db
sean miller <millertime@seans-MacBook-Pro.local>
2019-03-12 15:27:08 -0700

    Graph with remove edge  method

--- Graph/undirected_graph.js
+++ Graph/undirected_graph.js
@@ -8,9 +8,31 @@ class Graph {
       this.ajacencyList[vertex] = [];
     }
   }
+
+  addEdge(vertex1, vertex2) {
+    this.ajacencyList[vertex1].push(vertex2);
+    this.ajacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+
+    this.ajacencyList[vertex1] = this.ajacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+
+    this.ajacencyList[vertex2] = this.ajacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
+    
+  }
 }
 
 var g = new Graph();
 
 g.addVertex('pineapple');
+g.addVertex('apple');
+g.addVertex('watermelon');
+g.addEdge('pineapple', 'apple');
+g.addEdge('watermelon', 'apple');
+g.removeEdge('watermelon', 'apple');
 console.log(g);
